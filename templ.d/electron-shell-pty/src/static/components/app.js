fa2ac8798fe1dcbaf63e61d1843583081d04df36
Vladimir Barbaros <vladimir.b@bannernow.com>
2025-05-09 20:03:19 +0300

    update templ.d/electron-shell-pty/src/static/components/app.js

--- templ.d/electron-shell-pty/src/static/components/app.js
+++ templ.d/electron-shell-pty/src/static/components/app.js
@@ -1,13 +1,23 @@
 const app = Vue.createApp({
     template: `
         <div class="fix-f vsplit">
-            <div class="p5 mi5" style="background:#2c2c2c;">
-                <button v-on:click="exec('./artisan migrate')">migrate</button>
-                <button v-on:click="exec('./artisan migrate:rollback')">migrate:rollback</button>
-                <button v-on:click="exec('./artisan schema:dump')">schema:dump</button>
-                <button v-on:click="exec('git status')">git status</button>
-                <button v-on:click="exec('git add .; git stash')">git stash</button>
-                <button v-on:click="exec('git stash pop')">git stash pop</button>
+            <div class="p5 mg10" style="background:#2c2c2c;">
+                <div class="mi10">
+                    <button v-on:click="exec('./artisan migrate')">migrate</button>
+                    <button v-on:click="exec('./artisan migrate:rollback')">migrate:rollback</button>
+                    <button v-on:click="exec('./artisan schema:dump')">schema:dump</button>
+                </div>
+                <div class="mi10">
+                    <button v-on:click="exec('git status')">git status</button>
+                    <button v-on:click="exec('git add .; git stash')">git stash</button>
+                    <button v-on:click="exec('git stash pop')">git stash pop</button>
+                </div>
+                <div class="mi10">
+                    <button v-on:click="exec('docker compose up -d')">docker compose up -d</button>
+                    <button v-on:click="exec('docker compose down')">docker compose down</button>
+                    <button v-on:click="exec('docker compose logs')">docker compose logs</button>
+                    <button v-on:click="exec('docker compose ps')">docker compose ps</button>
+                </div>
             </div>
             <div class="fluid rel">
                 <xterm ref="xterm" class="abs-f black" />
