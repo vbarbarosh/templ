f82c3a10c4bad1a248e5f3fa656c865e573424ff
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-29 18:04:18 +0200

    update node-mysql-demos/index.js

--- templ.d/node-mysql-demos/index.js
+++ templ.d/node-mysql-demos/index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-require('@dotenvx/dotenvx').config();
-
 const cli = require('@vbarbarosh/node-helpers/src/cli');
 const mysql2 = require('mysql2/promise');
 
@@ -9,10 +7,12 @@ cli(main);
 
 async function main()
 {
-    const conn = await mysql2.createConnection('mysql://hello:hello@127.0.0.1/hello?debug=false&charset=utf8mb4&collation=utf8mb4_unicode_ci&timezone=0');
+    // const conn = await mysql2.createConnection('mysql://hello:hello@127.0.0.1/hello?debug=false&charset=utf8mb4&timezone=0');
+    const conn = await mysql2.createConnection('mysql://root:root@ignore/classicmodels?socketPath=../docker-compose-mysql/var-run-mysqld/mysqld.sock&debug=false&charset=utf8mb4&timezone=0');
     try {
-        const items = await conn.query('SELECT * FROM users LIMIT 10');
-        console.log(items);
+        const [items, defs] = await conn.query('SHOW FULL TABLES');
+        // node_modules/mysql2/lib/packets/column_definition.js
+        console.log({items, defs: defs.map(v => v.inspect())});
     }
     finally {
         await conn.end();
